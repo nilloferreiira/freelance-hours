4287983231f3e6338d576d772e66eecb196bfc61
Danillo <danillodana@outlook.com>
2024-10-10 18:36:44 -0300

    add create proposal and validation

--- app/Livewire/Projects/Show.php
+++ app/Livewire/Projects/Show.php
@@ -7,7 +7,6 @@ use App\Models\Project;
 
 class Show extends Component
 {
-
     public Project $project;
 
     public function render()
--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -20,7 +20,8 @@ class ProjectFactory extends Factory
     {
         return [
             'title' => collect(fake()->words(5))->join(' '),
-            'description' => htmlspecialchars(fake()->randomHtml()),
+            // 'description' => htmlspecialchars(fake()->randomHtml()),
+            'description' => 'oi',
             'ends_at' => fake()->dateTimeBetween('now', '+ 3 days'),
             'status' => fake()->randomElement(['open', 'closed']),
             'tech_stack' => fake()->randomElements(['react', 'nextjs', 'nodejs', 'javascript', 'vite'], random_int(1, 5)),
