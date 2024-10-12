7c23315964c811a57eb07083b1ef627739bb3fd4
Danillo <danillodana@outlook.com>
2024-10-12 00:39:03 -0300

    add notifications

---
+++ app/Notifications/NewProposal.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Project;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewProposal extends Notification
+{
+    use Queueable;
+
+    /**
+     * @var Project
+     */
+    protected $project;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param Project $project
+     */
+
+    public function __construct(Project $project)
+    {
+        $this->project = $project;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('Você recebeu uma nova proposta')
+            ->action('Veja seu projeto', route('projects.show', $this->project->id))
+            ->line('Obrigado!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
